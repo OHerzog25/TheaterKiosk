cdd0d31a1a51cf91c6311fa09a22cb9baba0b164
OHerzog25 <odes.xbox1@gmail.com>
2024-09-29 17:44:29 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int age = 5;
+
+        if (age > 20)
+        {
+            System.out.println("You get a wrist band!");
+        }
+    }
+}
